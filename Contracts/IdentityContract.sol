b9eeb7ac411b535e76762ef74d9f3cd1622096ed
Christoph Michelbach <michelbach94@gmail.com>
2019-12-10 15:36:08 +0100

    Enable all participants to have their own claim registries.

--- Contracts/IdentityContract.sol
+++ Contracts/IdentityContract.sol
@@ -1,7 +1,31 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 import "./erc725-735/contracts/Identity.sol";
 
 contract IdentityContract is Identity {
-
+    constructor
+    (
+        bytes32[] memory _keys,
+        uint256[] memory _purposes,
+        uint256 _managementRequired,
+        uint256 _executionRequired,
+        address[] memory _issuers,
+        uint256[] memory _topics,
+        bytes[] memory _signatures,
+        bytes[] memory _datas,
+        string[] memory _uris
+    ) Identity(
+            _keys,
+            _purposes,
+            _managementRequired,
+            _executionRequired,
+            _issuers,
+            _topics,
+            _signatures,
+            _datas,
+            _uris)
+        public {
+            
+        }
 }
--- Contracts/IdentityContractFactory.sol
+++ Contracts/IdentityContractFactory.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
-
 import "./IdentityContract.sol";
 import "./erc725-735/contracts/Identity.sol";
 import "./erc725-735/contracts/ERC735.sol";
@@ -9,8 +8,6 @@ import "./erc725-735/contracts/ERC735.sol";
 
 // Implements 735
 contract IdentityContractFactory {
-    Identity public identity;
-    
     mapping (address => bool) plantExistenceLookup;
     mapping (address => PlantType) plantTypeLookup;
     
@@ -22,29 +19,7 @@ contract IdentityContractFactory {
     enum ClaimType {IsBalanceAuthority, IsMeteringAuthority, IsPhysicalAssetAuthority, MeteringClaim, BalanceClaim, ExistenceClaim, GenerationTypeClaim, LocationClaim, IdentityContractFactoryClaim, EnergyTokenContractClaim, MarketRulesClaim, AcceptedDistributorContractsClaim }
     
 
-    constructor
-    (
-        bytes32[] memory _keys,
-        uint256[] memory _purposes,
-        uint256 _managementRequired,
-        uint256 _executionRequired,
-        address[] memory _issuers,
-        uint256[] memory _topics,
-        bytes[] memory _signatures,
-        bytes[] memory _datas,
-        string[] memory _uris
-    ) public {
-       identity = new Identity(
-        _keys,
-        _purposes,
-        _managementRequired,
-        _executionRequired,
-        _issuers,
-        _topics,
-        _signatures,
-        _datas,
-        _uris);
-        
+    constructor() public {
         owner = msg.sender;
     }
     
@@ -53,9 +28,9 @@ contract IdentityContractFactory {
         _;
     }
     
-    function registerAuthority(ClaimType _claimType, bytes memory _signature, bytes memory _data) public ownerOwnly {
+    function registerAuthority(address payable _authorityAddress, ClaimType _claimType, bytes memory _signature, bytes memory _data) public ownerOwnly {
         require(_claimType == ClaimType.IsBalanceAuthority || _claimType == ClaimType.IsMeteringAuthority || _claimType == ClaimType.IsPhysicalAssetAuthority);
-        identity.addClaim(claimType2Topic(_claimType), identity.ECDSA_SCHEME(), address(this), _signature, _data, "");
+        IdentityContract(_authorityAddress).addClaim(claimType2Topic(_claimType), IdentityContract(_authorityAddress).ECDSA_SCHEME(), msg.sender, _signature, _data, "");
     }
 
     function claimType2Topic(ClaimType _claimType) public pure returns (uint256 __topic) {
@@ -99,8 +74,28 @@ contract IdentityContractFactory {
         require(false);
     }
     
-    function createPlant(PlantType plantType) public {
-        IdentityContract plant = new IdentityContract();
+    function createPlant(
+        PlantType plantType,
+        bytes32[] memory _keys,
+        uint256[] memory _purposes,
+        uint256 _managementRequired,
+        uint256 _executionRequired,
+        address[] memory _issuers,
+        uint256[] memory _topics,
+        bytes[] memory _signatures,
+        bytes[] memory _datas,
+        string[] memory _uris
+        ) public {
+        IdentityContract plant = new IdentityContract(
+            _keys,
+            _purposes,
+            _managementRequired,
+            _executionRequired,
+            _issuers,
+            _topics,
+            _signatures,
+            _datas,
+            _uris);
 
         // Register plant.
         plantExistenceLookup[address(plant)] = true;
@@ -108,11 +103,11 @@ contract IdentityContractFactory {
         emit PlantCreation(plantType, address(plant), msg.sender);
     }
     
-    function isValidPlant(address plantAddress, PlantType plantType) public returns (bool) {
+    function isValidPlant(address plantAddress, PlantType plantType) public view returns (bool) {
         return isValidPlant(plantAddress) && (plantTypeLookup[plantAddress] == plantType);
     }
     
-    function isValidPlant(address plantAddress) public returns (bool) {
+    function isValidPlant(address plantAddress) public view returns (bool) {
         return plantExistenceLookup[plantAddress];
     }
 }
