ca4b0b3f0eb383a9b3bd7ab187c6c5de4e82e747
Christoph Michelbach <michelbach94@gmail.com>
2020-01-02 13:08:17 +0100

    Remove distributor subcontracts.

--- contracts/AbsoluteDistributor.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.0;
-import "./Distributor.sol";
-
-contract AbsoluteDistributor is Distributor {
-    constructor (address balanceAuthority_CAddress, address marketAuthorityAddress) Distributor(balanceAuthority_CAddress, marketAuthorityAddress, DistributorType.AbsoluteDistributor) public {
-        
-    }
-}
