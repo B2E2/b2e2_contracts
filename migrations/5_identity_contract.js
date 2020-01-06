e5d96b1e9be4fba2599248ead89905641a7401db
Christoph Michelbach <michelbach94@gmail.com>
2020-01-06 17:22:44 +0100

    Adapt deployment of IdentityContract.sol.

--- migrations/5_identity_contract.js
+++ migrations/5_identity_contract.js
@@ -6,6 +6,6 @@ const marketAuthority = artifacts.require("IdentityContract");
 module.exports = function(deployer) {
   deployer.link(ClaimCommons, marketAuthority);
   deployer.link(ClaimVerifier, marketAuthority);
-  deployer.deploy(marketAuthority, "0x0000000000000000000000000000000000000000", [], [], 1, 1, [], [], [], [], []);
+  deployer.deploy(marketAuthority, "0x0000000000000000000000000000000000000000");
 };
 
