3a4426ac8cc20a7473120d9cccc4aa47d4deade8
Christoph Michelbach <michelbach94@gmail.com>
2020-01-23 22:06:25 +0100

    Be more explicit about meaning of return value of getBalancePeriod().

--- contracts/Commons.sol
+++ contracts/Commons.sol
@@ -2,14 +2,17 @@ pragma solidity ^0.5.0;
 
 library Commons {
 
-    function getBalancePeriod() public view returns(uint64) {
+    /*
+    * Balance period does not start at 00:00:00 + i*15:00 but at 00:00:01 + i*15:00.
+    */
+    function getBalancePeriod() public view returns(uint64 __beginningOfBalancePeriod) {
         return getBalancePeriod(now);
     }
     
     /*
     * Balance period does not start at 00:00:00 + i*15:00 but at 00:00:01 + i*15:00.
     */
-    function getBalancePeriod(uint256 _timestamp) public pure returns(uint64) {
+    function getBalancePeriod(uint256 _timestamp) public pure returns(uint64 __beginningOfBalancePeriod) {
         _timestamp--;
         return uint64(_timestamp - (_timestamp % 900) + 1);
     }
