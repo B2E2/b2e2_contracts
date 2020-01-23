49fb51e41cc2bf2255969ddc400f4418af4a8ae4
Christoph Michelbach <michelbach94@gmail.com>
2020-01-23 22:03:48 +0100

    Implement ENBW version of balance period.

--- contracts/Commons.sol
+++ contracts/Commons.sol
@@ -6,7 +6,11 @@ library Commons {
         return getBalancePeriod(now);
     }
     
+    /*
+    * Balance period does not start at 00:00:00 + i*15:00 but at 00:00:01 + i*15:00.
+    */
     function getBalancePeriod(uint256 _timestamp) public pure returns(uint64) {
-        return uint64(_timestamp - (_timestamp % 900));
+        _timestamp--;
+        return uint64(_timestamp - (_timestamp % 900) + 1);
     }
 }
