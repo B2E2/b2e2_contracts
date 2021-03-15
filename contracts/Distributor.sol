0974866c26b3c5a7259404f337dbe8c65bcd197c
Christoph Michelbach <michelbach94@gmail.com>
2021-03-15 13:02:19 +0100

    Add comment.

--- contracts/Distributor.sol
+++ contracts/Distributor.sol
@@ -21,7 +21,9 @@ contract Distributor is IdentityContract {
     }
     
     function distribute(address payable _consumptionPlantAddress, uint256 _tokenId) external {
-        // Distributor applicability check
+        // Distributor applicability check. Required because this contract holding the necessary certificates to pay the consumption plant
+        // is not sufficient grouns to assume that this is the correct distributor as soon as several forwards may cause payout of the
+        // same certificates.
         require(energyToken.id2Distributor(_tokenId) == this, "Distributor contract does not belong to this _tokenId");
         
         // Single execution check
@@ -98,7 +100,9 @@ contract Distributor is IdentityContract {
     function withdrawSurplusCertificates(uint256 _tokenId) external {
         (IEnergyToken.TokenKind tokenKind, uint64 balancePeriod, address generationPlantAddress) = EnergyTokenLib.getTokenIdConstituents(_tokenId);
         
-        // Distributor applicability check
+        // Distributor applicability check. Required because this contract holding the necessary certificates to pay the consumption plant
+        // is not sufficient grouns to assume that this is the correct distributor as soon as several forwards may cause payout of the
+        // same certificates.
         require(energyToken.id2Distributor(_tokenId) == this, "Distributor contract does not belong to this _tokenId");
         
         // Single execution check
