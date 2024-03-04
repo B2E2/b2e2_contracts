2f08fcee2c42b900c4d7d95a7840a8331c7ce232
Christoph Michelbach <michelbach94@gmail.com>
2024-03-04 15:18:13 +0100

    Use only spaces for indentation.

--- migrations/3_libraries.js
+++ migrations/3_libraries.js
@@ -6,12 +6,12 @@ const EnergyTokenLib = artifacts.require('EnergyTokenLib');
 module.exports = function(deployer) {
     deployer.deploy(Commons).then(function() {
         return deployer.deploy(ClaimCommons).then(function() {
-	  deployer.link(Commons, ClaimVerifier);
-	  deployer.link(ClaimCommons, ClaimVerifier);
-	  return deployer.deploy(ClaimVerifier).then(function() {
+            deployer.link(Commons, ClaimVerifier);
+            deployer.link(ClaimCommons, ClaimVerifier);
+            return deployer.deploy(ClaimVerifier).then(function() {
                 deployer.link(ClaimVerifier, EnergyTokenLib);
                 return deployer.deploy(EnergyTokenLib);
-	  });
+            });
         });
     });
 };
