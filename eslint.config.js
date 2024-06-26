a7a4abe965fa5b336cd49161bf390dd9fe2b3db2
Christoph Michelbach <michelbach94@gmail.com>
2024-06-26 13:00:18 +0200

    Migrate to eslint version 9.
    - turn eslintrc into flat config file
    - move ignore rule flat config file
    - remove config of indent rule that seems to have changed
    - adapt npm scripts
    - install js defaults via @eslint/js because config inheritance is no longer supported

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
-module.exports = {
-    env: {
-        node: true,
-        commonjs: true,
-        es2021: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
+module.exports = [
+  {
+  files: ['**/*.js'],
+  ignores: ['dependencies/**'],
+  languageOptions: {
         ecmaVersion: 'latest',
-    },
     globals: {
         web3: 'readonly',
         artifacts: 'readonly',
@@ -16,9 +12,9 @@ module.exports = {
         it: 'readonly',
         assert: 'readonly',
         Buffer: 'readonly',
-    },
+    }
+  },
     rules: {
-        indent: ['error', 4],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
@@ -28,4 +24,8 @@ module.exports = {
             { 'vars': 'all', 'varsIgnorePattern': '^_', 'args': 'after-used', 'argsIgnorePattern': '^_' }
         ],
     },
-};
+  },
+  {
+    ignores: ['dependencies/**'],
+  }
+  ];
